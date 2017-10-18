2816e30be2afb51826dbff4306816c4a5edc86d4
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2017-10-18 15:07:08 +0200

    Implement Chunks

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -14,27 +14,27 @@
 
 namespace opossum {
 
-void Chunk::add_column(std::shared_ptr<BaseColumn> column) {
-  // Implementation goes here
-}
+void Chunk::add_column(std::shared_ptr<BaseColumn> column) { this->columns.push_back(std::move(column)); }
 
 void Chunk::append(std::vector<AllTypeVariant> values) {
-  // Implementation goes here
+  DebugAssert(this->columns.size() == values.size(), "#Values does not match #Columns");
+  for (uint i = 0; i < this->columns.size(); ++i) {
+    this->columns[i]->append(values[i]);
+  }
 }
 
 std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const {
-  // Implementation goes here
-  return nullptr;
+  return std::shared_ptr<BaseColumn>(this->columns[column_id]);
 }
 
-uint16_t Chunk::col_count() const {
-  // Implementation goes here
-  return 0;
-}
+uint16_t Chunk::col_count() const { return this->columns.size(); }
 
 uint32_t Chunk::size() const {
-  // Implementation goes here
-  return 0;
+  uint32_t maxRowCount = 0;
+  for (auto column : this->columns) {
+    maxRowCount = column->size() > maxRowCount ? column->size() : maxRowCount;
+  }
+  return maxRowCount;
 }
 
 }  // namespace opossum
